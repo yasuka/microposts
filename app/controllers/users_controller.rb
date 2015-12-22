789ed3bf8f69e8e23a0fe450954062be922c472a
yasuka <satoumirin7@gmail.com>
2015-12-22 14:02:36 +0000

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:show, :edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "登録ありがとうございます"
       redirect_to @user
     else
       render 'new'
@@ -35,5 +36,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :area, :password,
                                  :password_confirmation)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
